87ee1d70c73b7619903468178d65a2158b7fba19
SHRΞYΔSH <debnathswarup555@gmail.com>
2026-07-08 20:12:13 +0530

    Time: 30 ms (84.97%), Space: 155.3 MB (55.44%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -6,26 +6,25 @@ public:
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
         int n = s.length();
         
-        // 1. Precompute arrays
+        
         vector<int> nonZeroCount(n, 0);
         vector<ll> numberUpTo(n, 0);
         vector<ll> digitSumUpTo(n, 0);
         vector<ll> pow10(n + 1, 0);
 
-        // Fill pow10
+        
         pow10[0] = 1;
         for (int i = 1; i <= n; i++) {
             pow10[i] = (pow10[i - 1] * 10) % M;
         }
 
-        // Fill nonZeroCount
+        
         nonZeroCount[0] = (s[0] != '0') ? 1 : 0;
         for (int i = 1; i < n; i++) {
             int digit = s[i] - '0';
             nonZeroCount[i] = nonZeroCount[i - 1] + (digit != 0 ? 1 : 0);
         }
 
-        // Fill numberUpTo
         numberUpTo[0] = (s[0] - '0');
         for (int i = 1; i < n; i++) {
             int digit = s[i] - '0';
@@ -36,29 +35,29 @@ public:
             }
         }
 
-        // Fill digitSumUpTo
+        
         digitSumUpTo[0] = (s[0] - '0');
         for (int i = 1; i < n; i++) {
             digitSumUpTo[i] = (digitSumUpTo[i - 1] + (s[i] - '0'));
         }
 
-        // 2. Process Queries
+      
         int q = queries.size();
         vector<int> result(q);
         for (int i = 0; i < q; i++) {
             int L = queries[i][0];
             int R = queries[i][1];
 
-            // Get Sum
+            
             ll sum = digitSumUpTo[R] - (L > 0 ? digitSumUpTo[L - 1] : 0);
             
-            // Get X
+            
             ll numBefore = (L > 0 ? numberUpTo[L - 1] : 0);
             int k = nonZeroCount[R] - (L > 0 ? nonZeroCount[L - 1] : 0);
             
             ll x = (numberUpTo[R] - (numBefore * pow10[k]) % M + M) % M;
 
-            // Calculate Result
+        
             result[i] = (x * (sum % M)) % M;
         }
 
